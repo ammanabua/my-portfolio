4e3c57bbe13fd60913fab590eaa3d19950d88bbb
Amman Abua <ammanabua@gmail.com>
2021-07-28 10:30:01 +0100

    update form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -64,7 +64,7 @@ export default function Contact() {
           </div>
         </div>
         <form
-          data-netlify="true"
+          netlify
           name="contact"
           method="POST"
           className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
@@ -113,8 +113,7 @@ export default function Contact() {
           </div>
           <button
             type="submit"
-            className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg" 
-            onClick={handleSubmit}>
+            className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
             Submit
           </button>
         </form>
