cbf601f8c167b9f0379a6877b1c280e70ebb1764
Amman Abua <ammanabua@gmail.com>
2021-07-28 10:42:40 +0100

    update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 export default function Footer(){
     return(
-        <footer>
-            <p>THIS IS THE FOOTER</p>
+        <footer
+        className="bg-gray-800 bottom-0 z-10">
+            <p className="text-center">Copyright&copy; 2021 Amman Abua &reg; </p>
         </footer>
     )
 }
