4e9d632689ca85b4342136da298e9171b6101bdc
Amman Abua <ammanabua@gmail.com>
2021-08-07 14:02:28 +0100

    bug fix

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -125,7 +125,7 @@ export default function Contact() {
           <button
             type="submit"
             className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
-            onClick={resetInputField}>
+            >
             Submit
           </button>
         </form>
