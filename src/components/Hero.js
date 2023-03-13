9d37bfc5c077a6f4e9dbc4c83d62c49597a84764
Amman Abua <ammanabua@gmail.com>
2023-03-13 12:16:16 +0100

    add resume

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -73,9 +73,9 @@ export default function Hero (){
           Hire Me
           </a>
           <a
-          href="#projects"
+          href="/cv.pdf"
           className="ml-4 text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
-          Portfolio
+          Resume
           </a>
         </div>
       </div>
