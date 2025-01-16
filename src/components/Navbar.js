307b80f7fb277737fcd5fe4ae2f813336aea8c00
Amman Abua <ammanabua@gmail.com>
2025-01-16 11:42:01 -0600

    add navbar animation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,39 @@
 import { ArrowRightIcon } from "@heroicons/react/solid";
 import React, { useState, useEffect, useRef } from "react";
 import { Transition } from "@headlessui/react";
-import { Modal, Box, Typography } from "@mui/material";
+import { Modal, Box, Typography, backdropClasses } from "@mui/material";
+
+import { motion, } from "framer-motion";
+
+
+
+const parentVariants = {
+  visible: { opacity: 1, y: 0 },
+  hidden: { opacity: 0, y: "-4rem" },
+};
+
+const childVariants = {
+  visible: { opacity: 1, y: 0 },
+  hidden: { opacity: 0, y: "-2rem" },
+};
+
 
 
 export default function Navbar() {
 
   const [open, setOpen] = useState(false);
+
+  const [hidden, setHidden] = useState(false);
+
+  function update(latest, prev) {
+    if (latest < prev) {
+      setHidden(false);
+      // console.log("visible");
+    } else if (latest > 100 && latest > prev) {
+      setHidden(true);
+      // console.log("hidden");
+    }
+  }
 
   const [activeLink, setActiveLink] = useState("");
   const sectionRefs = useRef([]);
@@ -43,8 +70,19 @@ export default function Navbar() {
 
   
   return (
-    <header className="bg-transparent md:fixed w-full top-0 z-50">
-      <div className="container mx-auto flex p-5 items-center justify-between">
+    <header className="md:fixed w-full top-0 z-50">
+      <motion.div
+       initial={{ opacity: 0 }}
+       scroll
+       animate={{ opacity: 1, backgroundColor: "rgba(0, 0, 0, 0.5)" }}
+        transition={{
+        ease: [0.1, 0.25, 0.3, 1],
+        duration: 2,
+        staggerChildren: 0.05,
+        delay: 1
+        }}
+      
+       className="container mx-auto flex p-5 items-center justify-between">
         
         <div>
             <button onClick={() => scrollToSection("home")} className="ml-3 text-5xl justify-left title-font font-medium text-white mb-4 md:mb-0">
@@ -52,8 +90,8 @@ export default function Navbar() {
             </button>
         </div>
         <div>
-          <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 flex items-center text-white justify-center">
-            <div className="hidden md:block">
+          <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 flex items-center text-gray-200 justify-center">
+            <div className="hidden md:block text-lg">
               <a className={`${activeLink === "home" ? "active" : ""} ${"mr-5 hover:text-yellow-400"} cursor-pointer`} onClick={() => scrollToSection("home")}>
                 Home
               </a>
@@ -92,7 +130,7 @@ export default function Navbar() {
             </svg>
           </button>
         </div>
-      </div>
+      </motion.div>
       
       <Modal
             open={open}
